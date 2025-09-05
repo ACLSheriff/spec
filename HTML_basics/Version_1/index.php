9aeda52c03296ca8d3bf550f2a6b882f6db35d0c
22sherifa <ava.sheriff@icloud.com>
2025-09-05 10:41:15 +0100

    practice website

---
+++ HTML_basics/Version_1/index.php
@@ -0,0 +1,16 @@
+<?php // this opens php code section will interprate
+
+echo "<html>";
+echo "<head>";
+echo "<title> Ava </title>";
+
+echo "</head>";
+echo "<body>";
+
+echo "<h1> Ava </h1>";
+
+echo "hellloooo.";
+
+echo "</body>";
+echo "</html>";
+?>
