60884ecbf7efaedc62f823898a6d774b8746c25a
22sherifa <ava.sheriff@icloud.com>
2025-09-05 13:13:22 +0100

    practice website

--- HTML_basics/Version_1/index.php
+++ HTML_basics/Version_1/index.php
@@ -1,16 +1,42 @@
 <?php // this opens php code section will interprate
 
+echo "<!DOCTYPE html>";
+
 echo "<html>";
-echo "<head>";
-echo "<title> Ava </title>";
+    echo "<head>";
+        echo "<title> Ava </title>";
 
-echo "</head>";
-echo "<body>";
+    echo "</head>";
+    echo "<body>";
 
-echo "<h1> Ava </h1>";
+        echo "<h1> Ava </h1>";
 
-echo "hellloooo.";
+        echo "helllooooooooooooo<br>ooooooooooooooooo.";
 
-echo "</body>";
+        echo "<p> helooo im here </p>";
+        echo "<b> this is bold </b>";
+        echo "<hr>";
+        echo "<i> hellooo </i>";
+
+        echo "<a href='page2.php'> go to page2 </a>";
+
+        echo "<img scr='computer.jfif'>";
+
+        echo "<table>";
+
+            echo "<tr>";
+                echo"<td> hello1 </td>";
+                echo"<td> hello2 </td>";
+                echo"<td> hello3 </td>";
+            echo "</tr>";
+            echo "<tr>";
+                echo"<td> 5 </td>";
+                echo"<td> 6 </td>";
+                echo"<td> 7 </td>";
+
+        echo "</table>";
+
+
+    echo "</body>";
 echo "</html>";
 ?>
