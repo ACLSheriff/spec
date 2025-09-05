75aca1a32989dfbf797995e7bf7b9961038bf34d
22sherifa <ava.sheriff@icloud.com>
2025-09-05 14:32:57 +0100

    practice website

--- HTML_basics/Version_1/index.php
+++ HTML_basics/Version_1/index.php
@@ -9,23 +9,23 @@ echo "<html>";
     echo "</head>";
     echo "<body>";
 
-        echo "<h1> Ava </h1>";
+        echo "<h1> Ava </h1>";// heading
 
-        echo "helllooooooooooooo<br>ooooooooooooooooo.";
+        echo "helllooooooooooooo<br>ooooooooooooooooo.";// word with a break in
 
-        echo "<p> helooo im here </p>";
-        echo "<b> this is bold </b>";
-        echo "<hr>";
-        echo "<i> hellooo </i>";
+        echo "<p> helooo im here </p>";// paragh writing
+        echo "<b> this is bold </b>"; // bold text
+        echo "<hr>";// line in the page
+        echo "<i> hellooo </i>";// makes the text wonky
 
-        echo "<a href='page2.php'> go to page2 </a>";
+        echo "<a href='page2.php'> go to page2 </a>";// hyperlink
 
-        echo "<img scr='computer.jfif'>";
+        echo "<img src='computer.jfif'>";// to do a image
 
-        echo "<table>";
+        echo "<table>";// to start make a table
 
             echo "<tr>";
-                echo"<td> hello1 </td>";
+                echo"<td> hello1 </td>";// items in table
                 echo"<td> hello2 </td>";
                 echo"<td> hello3 </td>";
             echo "</tr>";
@@ -35,6 +35,18 @@ echo "<html>";
                 echo"<td> 7 </td>";
 
         echo "</table>";
+
+        echo "<ul>";// make a list with bullit points
+            echo "<li> this is item 1 </li>";
+            echo "<li> this is item 1 </li>";
+
+        echo "</ol>";
+
+        echo "<ul>";// makea list with numbers
+            echo "<li> this is item 1 </li>";
+            echo "<li> this is item 1 </li>";
+
+        echo "</ol>";
 
 
     echo "</body>";
---
+++ HTML_basics/Version_1/page 1.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "<!DOCTYPE html>";
+
+echo "<html>";
+    echo "<head>";
+        echo "<title> this is Asda </title>";
+
+    echo "</head>";
+    echo "<body>";
+
+
+        echo "<img src='asda.jfif'>";
+        echo "<hr>";
+
+        echo "<a href='page2.php'> here is our competior </a>";
+
+        echo "</ol>";
+
+            echo "<ul>";// makea list with numbers
+            echo "<li> this is item 1 </li>";
+            echo "<li> this is item 1 </li>";
+
+        echo "</ol>";
+
+
+    echo "</body>";
