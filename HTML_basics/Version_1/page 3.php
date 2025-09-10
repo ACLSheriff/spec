e16c76faee7a4c565ad2d4bd11a2ddbf27f81ebc
avasheriff <ava.sheriff@icloud.com>
2025-09-10 21:10:45 +0100

    added pages that im making to practice making website pages with content, linked togther, supermarket pages, here are coding chenges done to these files

--- HTML_basics/Version_1/page 3.php
+++ HTML_basics/Version_1/page 3.php
@@ -1,1 +1,29 @@
 <?php
+
+    echo "<html>";
+        echo "<head>";
+            echo "<title> this is Tesco </title>";
+
+        echo "</head>";
+        echo "<body>";
+
+
+            echo "<img src='Tesco.jfif'>";
+            echo "<hr>";
+            echo "<b> hello we are Tesco  </b>";
+            echo "<a href='page4.php'> here is our competior </a>";
+
+
+            echo "<ul>";// makea list with bullet points
+                echo "<li> we sell </li>";
+                echo "<li> ready meals </li>";
+                echo "<li> collin the caltiplier  </li>";
+                echo "<li> amazing bakery </li>";
+                echo "<li> and much more </li>";
+            echo "</ul>";
+
+
+        echo "</body>";
+    echo "</html>";
+
+?>
--- HTML_basics/Version_1/page 4.php
+++ HTML_basics/Version_1/page 4.php
@@ -1,1 +1,29 @@
 <?php
+
+
+    echo "<html>";
+    echo "<head>";
+        echo "<title> this is Tesco </title>";
+
+        echo "</head>";
+    echo "<body>";
+
+
+        echo "<img src='Tesco.jfif'>";
+        echo "<hr>";
+        echo "<b> hello we are Tesco  </b>";
+        echo "<p> we are a competior </p>";
+
+
+        echo "<ul>";// makea list with bullet points
+            echo "<li> we sell </li>";
+            echo "<li> ready meals </li>";
+            echo "<li> collin the caltiplier  </li>";
+            echo "<li> amazing bakery </li>";
+            echo "<li> and much more </li>";
+        echo "</ul>";
+
+        echo "</body>";
+    echo "</html>";
+
+?>
