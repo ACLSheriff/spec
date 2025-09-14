a282bed1559456a4c4808398c11a046a446448f4
avasheriff <ava.sheriff@icloud.com>
2025-09-14 17:13:49 +0100

    supermarket pages,adding comments in

--- HTML_basics/Version_1/page 4.php
+++ HTML_basics/Version_1/page 4.php
@@ -1,32 +1,30 @@
 <?php
 
-
     echo "<html>";
     echo "<head>";
-        echo "<title> this is Co-op </title>";
-        echo "<link rel='stylesheet' type='text/css' href='css/page_4.css' />";
+        echo "<title> this is Co-op </title>";//title of the page
+        echo "<link rel='stylesheet' type='text/css' href='css/page_4.css' />";//connects to a style sheet
         echo "</head>";
-        echo "<body>";
+        echo "<body>";//body of page
 
+        echo "<img src='Co-Op.jfif'>";// displays an image
+        echo "<hr>";// creats a line break in page
+        echo "<b> hello we are Co-op  </b>";// bold text
+        echo "<p> we are a competior </p>";// paragh text
 
-        echo "<img src='Co-Op.jfif'>";
-        echo "<hr>";
-        echo "<b> hello we are Co-op  </b>";
-        echo "<p> we are a competior </p>";
-
-        echo "<hr>";
-        echo "<i> hi we are a local shop, we sell good food, Ava approved, sells basics and good bits</i>";
-        echo "<br>";
+        echo "<hr>";// line break in page
+        echo "<i> hi we are a local shop, we sell good food, Ava approved, sells basics and good bits</i>";//wonky text
+        echo "<br>";// break in the page
 
         echo "<ul>";// makea list with bullet points
-            echo "<li> We Sell: </li>";
+            echo "<li> We Sell: </li>";// items in list
             echo "<li> crisps </li>";
             echo "<li> jelly </li>";
             echo "<li> fruit </li>";
         echo "</ul>";
 
-        echo "<hr>";
-        echo "<p> we also do funalras </p>";
+        echo "<hr>";// break with a line in the page
+        echo "<p> we also do funalras </p>";// paragh of text
 
         echo "</body>";
     echo "</html>";
