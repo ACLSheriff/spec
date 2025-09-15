26a4168826f3007cae133c4a3b0a597240acca58
22sherifa <ava.sheriff@icloud.com>
2025-09-15 10:49:45 +0100

    added and coded home page

--- HTML_basics/project_2/index.php
+++ HTML_basics/project_2/index.php
@@ -1,1 +1,33 @@
 <?php
+
+echo "<!DOCTYPE html>";
+    echo "<html>";
+        echo "<head>";
+            echo "<title> project 2 </title>";
+            echo "<link rel='stylesheet' type='text/css' href='css/stylesheet.css'/>";
+        echo "</head>";
+        echo "<body>";
+
+            echo "<div class='container'>";
+
+                require_once "assests/topBar.php";
+                require_once "assests/nav_bar.php";
+
+            echo "<div class='content'>";
+                echo "<br>";
+                echo "<h2> GibJohn Tutoring </h2>";
+
+                echo "<p class='content'> Welcome to GibJohn Tutoring – your personalized learning experience! </p> ";
+                echo "<br>";
+                echo "<p class='content'> We offer interactive tutoring sessions and a wide range of educational resources to help students thrive.</p>";
+                echo "<br>";
+                echo "<p class='content'> Our platform supports collaborative learning, provides real-time progress tracking, <br> and integrates rewarding gamified features to make learning engaging and fun.</p>";
+                echo "<br>";
+
+                echo "<img src='images/peopleTutoring.jfif'>";
+
+            echo "</div>";
+            echo "</div>";
+        echo "</body>";
+    echo "</html>";
+?>
