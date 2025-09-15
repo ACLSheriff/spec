7432f11ee85472d080a296bda17d01cb610a3927
22sherifa <ava.sheriff@icloud.com>
2025-09-15 12:21:37 +0100

    added to code for about section

---
+++ template/index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<!DOCTYPE html>";/*required tag*/
+echo "<html>";/*opens page content*/
+echo "<head>";/*opens the head of the code */
+
+echo "<title> insert title </title>";/*titles the page*/
+echo "<link rel='stylesheet' type='text/css' href='css/stylesheet.css'/>";//links to style sheet
+
+echo "</head>";// closes the head of the page
+echo "<body>";//opens the body of the page
+
+echo "<div class='container'>";//dive alows you to split your page up and class allows you to style that div
+
+require_once "assests/topBar.php";// gets and displays the top bar
+require_once "assests/nav_bar.php";
+
+echo "<div class='content'>";
+
+echo "</div>";
+echo "</div>";
+echo "<body>";
+echo "<html>";
+?>
