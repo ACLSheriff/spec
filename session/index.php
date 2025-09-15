344de649bb6031ff174b004f400e70ee2b6985f3
22sherifa <ava.sheriff@icloud.com>
2025-09-15 14:17:01 +0100

    making a session

--- session/index.php
+++ session/index.php
@@ -1,14 +1,10 @@
 <?php
-
 session_start();
-if ($_SEVER[''])
-
-
 echo "<!DOCTYPE html>";/*required tag*/
 echo "<html>";/*opens page content*/
 echo "<head>";/*opens the head of the code */
 
-echo "<title> enter title </title>";/*titles the page*/
+echo "<title> insert title </title>";/*titles the page*/
 echo "<link rel='stylesheet' type='text/css' href='css/stylesheet.css'/>";//links to style sheet
 
 echo "</head>";// closes the head of the page
@@ -20,12 +16,6 @@ require_once "assests/topBar.php";// gets and displays the top bar
 require_once "assests/nav_bar.php";
 
 echo "<div class='content'>";
-
-echo "<h2> session work </h2>";
-echo "<form method = 'post' action = ''> <br>";
-echo "<input type = 'text' name = 'massage' placeholder='Enter a message here'>";
-echo "<input type = 'submit' name = 'submit' value = 'submit'>";
-echo "</form>";
 
 echo "</div>";
 echo "</div>";
