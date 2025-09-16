d2654c3acb86af733e6178319099f4054f5de242
22sherifa <ava.sheriff@icloud.com>
2025-09-16 11:57:08 +0100

    making a session and seeing if a good or bad request and changing colour depeding on request

--- session/assets/common.php
+++ session/assets/common.php
@@ -7,9 +7,12 @@ Function user_msg(){//creates a function
     if(isset($_SESSION['msg'])){// checks if session messge have been sset
 
         if (str_contains($_SESSION['msg'],"error")) {
+            $msg = "<div id='error'> USER MESSAGE".$_SESSION['msg']."</div>";
 
+        } else {
+            $msg = "<div id='umsg'> USER MESSAGE".$_SESSION['msg']."</div>";
         }
-        $msg = 'USER MESSAGE: '.$_SESSION['msg'];// creats a messge to echo out
+
         $_SESSION['msg'] = '';
         unset($_SESSION['msg']);//unset so it dosnt display else where
         return $msg;//returns
