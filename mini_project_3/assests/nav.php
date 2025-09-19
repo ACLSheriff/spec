61f59b88f8b4258117d5d46feb85a7f00e11495e
22sherifa <ava.sheriff@icloud.com>
2025-09-19 10:12:02 +0100

    starting new project 3 added added some code to sections or just editing the top bar, adding the templates in and the design folder

--- mini_project_3/assests/nav.php
+++ mini_project_3/assests/nav.php
@@ -8,7 +8,6 @@ echo "<ul>";  // opens the list
 echo "<li> <a href='index.php'>Home</a> </li>";//links of each page that can be displayed to click through
 echo "<li> <a href='password_checker.php'>password checker</a> </li>";
 
-
 echo "</ul>";  // closes the row of the list.
 
 echo "</nav>";  // closes off the naviagtion links
