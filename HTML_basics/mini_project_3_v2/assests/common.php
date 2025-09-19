d2e4795f0a2bddacd3ff4c1387806be9a1ed7c92
22sherifa <ava.sheriff@icloud.com>
2025-09-19 14:20:32 +0100

    project 3 finnshed i have made the design and the layout for the checker and the homepage is done ready to start coding the password cheker

---
+++ HTML_basics/mini_project_3_v2/assests/common.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+Function checker(){//creates a function
+
+    if(isset($_POST['msg'])){// checks if session messge have been sset
+
+
+    }
+    else {
+        return "";
+    }
+
+}
